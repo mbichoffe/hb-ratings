30185a7d6b6d69e9f1a10cd035df7c4a20948974
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-20 13:09:23 -0700

    added movie and user info pages with update ratings feature

--- server.py
+++ server.py
@@ -47,11 +47,11 @@ def process_registration():
     email = request.form.get('email')
     password = request.form.get('password')
     zipcode = request.form.get('zipcode')
-    age = int(request.form.get('age'))
+    age = request.form.get('age')
 
-    q = User.query.filter_by(email=email)
+    user = User.query.filter_by(email=email).first()
 
-    if q.first():
+    if user:
         flash("You already registered")
         return redirect("/")
 
@@ -65,12 +65,12 @@ def process_registration():
 
         db.session.commit()
         flash("Successfully registered!")
-        return redirect("/")
+        return redirect("/users/{}".format(user.user_id))
 
 
 @app.route('/process_login', methods=['POST'])
 def process_login():
-
+    """Checks if user exists and verifies password."""
     email = request.form.get('email')
     password = request.form.get('password')
 
@@ -82,7 +82,7 @@ def process_login():
             session['user_id'] = user.user_id
 
             flash('Logged in!')
-            return redirect('/')
+            return redirect('/users/{}'.format(user.user_id))
         flash("Invalid password. Try again.")
         return redirect('/')
     else:
@@ -91,10 +91,84 @@ def process_login():
 
 @app.route('/process_logout', methods=['POST'])
 def process_logout():
+    """Ends current session."""
 
     del session['user_id']
     flash('logged out successfully')
     return redirect('/')
+
+@app.route('/users/<user_id>')
+def user_details(user_id):
+    """Displays user details and movies rated by user"""
+
+    user = User.query.filter_by(user_id=user_id).first()
+    ratings = user.ratings
+    age = user.age
+    zipcode = user.zipcode
+
+
+    return render_template('user_info.html',
+                            user_id=user.user_id,
+                            ratings=ratings,
+                            age=age,
+                            zipcode=zipcode)
+
+@app.route('/movies')
+def lists_movies():
+    """Displays a list of all the movies."""
+
+    movies = Movie.query.order_by(Movie.title).all()
+
+    return render_template('movies.html', movies=movies)
+
+@app.route('/movies/<movie_id>')
+def movie_details(movie_id):
+    """Displays movie details"""
+
+    movie = Movie.query.filter_by(movie_id=movie_id).first()
+    title = movie.title
+    released_at = movie.released_at
+    imdb_url = movie.imdb_url
+    ratings = movie.ratings
+
+    return render_template("movie_info.html",
+                            movie_id=movie.movie_id,
+                            title=title,
+                            released_at=released_at,
+                            imdb_url=imdb_url,
+                            ratings=ratings)
+
+
+@app.route('/rate_movie', methods=['POST'])
+def rate_movie():
+    """Updates database with new rating."""
+
+    #query database for existing rating by filtering by user_id/movie_id
+
+    #User is adding a new rating
+    rating = request.form.get('rating')
+    movie_id = request.form.get('movie_id')
+    user_id = request.form.get('user_id')
+
+    existing_rating = Rating.query.filter(Rating.user_id == user_id,
+                                          Rating.movie_id == movie_id).first()
+    #update existing rating, if true
+    if existing_rating:
+        existing_rating.score = rating
+
+
+    else:
+        new_rating = Rating(user_id=int(user_id),
+                            movie_id=int(movie_id),
+                            score=rating)
+
+        db.session.add(new_rating)
+
+    db.session.commit()
+
+    return redirect('/movies/{}'.format(movie_id))
+
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
